3e763ff579b3e39b9037c86ebf4f9eae570ab719
Biveek-khatiwada <157800327+Biveek-khatiwada@users.noreply.github.com>
2025-12-03 11:59:57 +0545

    Updated employee view and homepage layout

--- myapp/views.py
+++ myapp/views.py
@@ -2,22 +2,26 @@ from django.http import HttpResponse
 from django.shortcuts import render
 import datetime
 from emp.models import CustomUser
+from django.db.models import Count
 
 def home_page(request):
     try:
-        
         emp_count = CustomUser.objects.count()
-        emp_details = CustomUser.objects.all() 
+        emp_details = CustomUser.objects.select_related('user').all()
         
         context = {
             'emp_details': emp_details,
             'emp_count': emp_count,
+            'current_time': datetime.datetime.now(),  # Add this line
         }
         
-        for i in emp_details:
-            print(i.phone_number)
         return render(request, 'emp/home.html', context)
     
     except Exception as e:
         print(f"Error in home_page: {e}")
-        return render(request, 'emp/home.html', {'emp_details': [], 'emp_count': 0})
+        context = {
+            'emp_details': [],
+            'emp_count': 0,
+            'current_time': datetime.datetime.now(),  # Add this line for error case too
+        }
+        return render(request, 'emp/home.html', context)
