09f6336ba14bbe9a574aa9ea41fe6c835a5c45f9
biswas-bit <pokhrelbiswas53@gmail.com>
2026-01-17 18:57:46 +0545

    added urls to that view function

--- emp/urls.py
+++ emp/urls.py
@@ -5,6 +5,7 @@ app_name = "emp"
 urlpatterns = [
     path('', views.home_page, name='home_page'),
     path('add-employee/', views.add_employee, name='add_employee'),
+    path('manage-departments/',views.manage_departments,name='manage_departments')
 ]
 
 
