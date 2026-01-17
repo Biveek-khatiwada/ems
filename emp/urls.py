702e5a18d3c07f06cc08f37440eaaa73a8e963c8
biswas-bit <pokhrelbiswas53@gmail.com>
2026-01-17 20:47:38 +0545

    updated urls

--- emp/urls.py
+++ emp/urls.py
@@ -5,11 +5,11 @@ app_name = "emp"
 urlpatterns = [
     path('', views.home_page, name='home_page'),
     path('add-employee/', views.add_employee, name='add_employee'),
-    path('manage-departments/',views.manage_departments,name='manage_departments'),
+    path('manage-departments/', views.manage_departments, name='manage_departments'),
+    path('departments/get/<int:department_id>/', views.get_department_data, name='get_department_data'),
     path('departments/add/', views.add_department, name='add_department'),
-    path('departments/<uuid:department_id>/edit/', views.edit_department, name='edit_department'),
-     path('departments/<uuid:department_id>/delete/', views.delete_department, name='delete_department'),
-    
+    path('departments/edit/<int:department_id>/', views.edit_department, name='edit_department'),
+    path('departments/delete/<int:department_id>/', views.delete_department, name='delete_department'),
 ]
 
 
