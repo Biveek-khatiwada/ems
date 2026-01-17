4e758ae4d46896b099083b7c9f280c246c4dd2a5
biswas-bit <pokhrelbiswas53@gmail.com>
2026-01-17 19:32:30 +0545

    urls for add_department function

--- emp/urls.py
+++ emp/urls.py
@@ -5,7 +5,8 @@ app_name = "emp"
 urlpatterns = [
     path('', views.home_page, name='home_page'),
     path('add-employee/', views.add_employee, name='add_employee'),
-    path('manage-departments/',views.manage_departments,name='manage_departments')
+    path('manage-departments/',views.manage_departments,name='manage_departments'),
+    path('departments/add/', views.add_department, name='add_department'),
 ]
 
 
